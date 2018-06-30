f26d023965ec781cfafb19528a5303d379581e11
Danilo-Zekovic <danilozeka93@gmail.com>
2018-06-30 12:34:46 +0200

    expanded the logic and complexity

--- app.js
+++ app.js
@@ -10,41 +10,75 @@ var app = express();
 const server = http.createServer(app)
 
 // Fake database ====
+const agents = [
+  { id: 1, firstName: 'Larry', middleInitial: 'L', lastName: 'Thomas'},
+  { id: 2, firstName: 'Bill', middleInitial: 'R', lastName: 'Lewis' }
+]
+
 const authors = [
-  { id: 1, firstName: 'Tom', middle: 'William',lastName: 'Coleman' },
-  { id: 2, firstName: 'Sashko', middle: 'Lester', lastName: 'Stubailo' },
-  { id: 3, firstName: 'Mikhail', middle: 'Leo', lastName: 'Novikov' },
+  { id: 1, firstName: 'Tom', middleInitial: 'W',lastName: 'Coleman', agent: find(agents,{id: 1})},
+  { id: 2, firstName: 'Sashko', middleInitial: 'L', lastName: 'Stubailo', agent: find(agents,{id: 2})},
+  { id: 3, firstName: 'Mikhail', middleInitial: 'R', lastName: 'Novikov', agent: find(agents, {id: 1}) },
 ];
 
 const posts = [
-  { id: 1, authorId: 1, title: 'Introduction to GraphQL' },
-  { id: 2, authorId: 2, title: 'GraphQL Rocks' },
-  { id: 3, authorId: 2, title: 'Advanced GraphQL' },
-  { id: 4, authorId: 3, title: 'Amazing GraphQL' },
-  { id: 5, authorId: 1, title: 'Get Started with Apollo'},
+  { id: 1, authorId: 1, title: 'Introduction to GraphQL',articleType: 'REVIEW' },
+  { id: 2, authorId: 2, title: 'GraphQL Rocks', articleType:  'OPINION' },
+  { id: 3, authorId: 2, title: 'Advanced GraphQL', articleType: 'TECHNICAL' },
+  { id: 4, authorId: 3, title: 'Launchpad is Cool', articleType: 'OPINION' },
 ];
 
 // ====
 
 const typeDefs = `
-  type Author {
-    id: Int!
+  interface Person {
+    id: ID!
     firstName: String
-    middle: String
+    middleInitial: String
     lastName: String
-    posts: [Post]
   }
 
-  type Post {
+  enum PostType {
+    NEWS
+    SPORTS
+    OPINION
+    REVIEW
+    ANALYSIS
+    TECHNICAL
+  }
+
+  type Author implements Person {
+    id: ID!
+    firstName: String
+    middleInitial: String
+    lastName: String
+    posts: [Post]
+    agent: Agent
+  }
+
+  type Agent implements Person {
+    id: ID!
+		firstName: String
+	  middleInitial: String
+    lastName: String
+		represents: [Author]
+	}
+
+	type Post {
     id: Int!
     title: String
     author: Author
+		articleType: PostType
+
   }
 
   type Query {
     posts: [Post]
     authors: [Author]
+		agents: [Agent]
+		people: [Person]
     author(id: Int!): Author
+		agent(id: Int!): Agent
   }
 `;
 
@@ -52,6 +86,9 @@ const resolvers = {
   Query: {
     posts: () => posts,
     authors: () => authors,
+    agents: () => agents,
+    people: () => authors.concat(agents),
+    agent: (_, args) => find(agents,{ id: args.id }),
     author: (_, args) => find(authors, { id: args.id }),
   },
   Author: {
@@ -59,6 +96,18 @@ const resolvers = {
   },
   Post: {
     author: (post) => find(authors, { id: post.authorId }),
+  },
+  Agent: {
+    represents: (agent) => filter(authors, { agent: agent})
+  },
+		Person: {
+    __resolveType(data, context, info) {
+		  if (data.agent) {
+        return 'Author'
+      } else {
+        return 'Agent'
+      }
+    }
   },
 };
 
