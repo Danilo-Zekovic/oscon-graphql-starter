df84edce9df8178f7b73d35e2cb2681765099648
Brian Capouch <brianc@palaver.net>
2018-07-12 00:45:12 -0400

    Provide simple "Hell world" functionality

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
-import { buildSchema } from 'graphql'
-import { find, filter } from 'lodash';
-import { makeExecutableSchema } from 'graphql-tools';
 import http from 'http'
 
-// Fake data
-const authors = [
-  { id: 1, firstName: 'Tom', middle: 'William',lastName: 'Coleman' },
-  { id: 2, firstName: 'Sashko', middle: 'Lester', lastName: 'Stubailo' },
-  { id: 3, firstName: 'Mikhail', middle: 'Leo', lastName: 'Novikov' },
-];
-
-const posts = [
-  { id: 1, authorId: 1, title: 'Introduction to GraphQL' },
-  { id: 2, authorId: 2, title: 'GraphQL Rocks' },
-  { id: 3, authorId: 2, title: 'Advanced GraphQL' },
-  { id: 4, authorId: 3, title: 'Amazing GraphQL' },
-  { id: 5, authorId: 1, title: 'Get Started with Apollo'},
-];
-
-// TODO: GraphQL queries schemas...
-
-
-
+// The simplest express server on earth
 var app = express();
 const server = http.createServer(app)
 
-// TODO: app use GraphQL
-
+app.get('/', (req, res) => res.send('Hello World!'))
 
 server.listen(4000);
 console.log(
