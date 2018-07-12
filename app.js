750985718c371a36b8ac250b701f97717089d731
Brian Capouch <brianc@palaver.net>
2018-07-12 17:23:11 -0400

    Move data outboard

--- app.js
+++ app.js
@@ -5,30 +5,12 @@ import { find, filter } from 'lodash';
 import { makeExecutableSchema } from 'graphql-tools';
 import http from 'http'
 
+// Get data
+import { authors, agents, posts } from "./data"
+
 // init express app and http server
 var app = express();
 const server = http.createServer(app)
-
-// "Mock" data ====
-const agents = [
-  { id: 1, firstName: 'Larry', middleInitial: 'L', lastName: 'Thomas'},
-  { id: 2, firstName: 'Bill', middleInitial: 'R', lastName: 'Lewis' }
-]
-
-const authors = [
-  { id: 1, firstName: 'Tom', middleInitial: 'W',lastName: 'Coleman', agent: find(agents,{id: 1})},
-  { id: 2, firstName: 'Sashko', middleInitial: 'L', lastName: 'Stubailo', agent: find(agents,{id: 2})},
-  { id: 3, firstName: 'Mikhail', middleInitial: 'R', lastName: 'Novikov', agent: find(agents, {id: 1}) },
-];
-
-const posts = [
-  { id: 1, authorId: 1, title: 'Introduction to GraphQL',articleType: 'REVIEW' },
-  { id: 2, authorId: 2, title: 'GraphQL Rocks', articleType:  'OPINION' },
-  { id: 3, authorId: 2, title: 'Advanced GraphQL', articleType: 'TECHNICAL' },
-  { id: 4, authorId: 3, title: 'Launchpad is Cool', articleType: 'OPINION' },
-];
-
-// ====
 
 const typeDefs =
 `
