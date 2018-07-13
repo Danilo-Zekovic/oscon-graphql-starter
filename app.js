9c57b3e32c30b3325466ca8f61c1d13cb1e2cbc2
Brian Capouch <brianc@palaver.net>
2018-07-13 01:42:09 -0400

    Remove extraneous code

--- app.js
+++ app.js
@@ -39,22 +39,6 @@ const typeDefs =
   }
 `;
 
-/*
-const resolvers = {
-  Query: {
-    posts: () => posts,
-    authors: () => authors,
-    author: (_, args) => find(authors, { id: args.id }),
-  },
-  Author: {
-    posts: (author) => filter(posts, { authorId: author.id }),
-  },
-  Post: {
-    author: (post) => find(authors, { id: post.authorId }),
-  },
-};
-*/
-
 // Compile schema
 export const schema = makeExecutableSchema({
   typeDefs,
