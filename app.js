a66c0ab9318ef9290531af8baa3f0f68768d7356
Brian Capouch <brianc@palaver.net>
2018-07-13 02:27:23 -0400

    Modify for outboard resolvers

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ var app = express()
 // Bring in our mock data
 import { agents, authors, posts } from "./data"
 
+// And the resolvers
+import { resolvers } from './resolvers'
+
 const typeDefs =
 `
   interface Person {
@@ -94,66 +97,6 @@ const typeDefs =
       postAdded: Post
   }
 `
-
-const resolvers = {
-  Query: {
-    posts: () => posts,
-    authors: () => authors,
-    agents: () => agents,
-    people: () => authors.concat(agents),
-    agent: (_, args) => find(agents,{ id: args.id }),
-    author: (_, args) => find(authors, { id: args.id }),
-  },
-
-  Mutation: {
-    createPost: (_, {input}) => {
-      // Build new post object from input values
-
-      // "id" is sequential index into "posts" array
-      let id = posts.length + 1
-
-      // Extract values from input data
-      let thisTitle = input["title"]
-      let thisArticleType = input["articleType"]
-      let thisAuthor = input["authorId"]
-
-      let newPost = {id: id, title: thisTitle, authorId: thisAuthor, articleType: thisArticleType}
-      posts.push(newPost)
-      // Notify subscribers
-      pubsub.publish(POST_ADDED_TOPIC, { postAdded: newPost });  // publish to a topic
-      return newPost
-      },
-    },
-
-  Subscription: {
-    postAdded: {
-      subscribe: () => pubsub.asyncIterator(POST_ADDED_TOPIC)  // subscribe to new posts
-    }
-  },
-
-  Author: {
-    posts: (author) => filter(posts, { authorId: author.id }),
-  },
-  Post: {
-    author: (post) => find(authors, { id: post.authorId }),
-  },
-  Agent: {
-    represents: (agent) => filter(authors, { agent: agent})
-  },
-	Person: {
-    // This code differentiates between the two implementations of Person
-    // It is required because objects returned from the "people" query
-    //    must be defined types, not interface types
-    __resolveType(data, context, info) {
-		  if (data.agent) {
-        return 'Author'
-      } else {
-        return 'Agent'
-      }
-    }
-  },
-}
-
 // Create the schema object used below
 export const schema = makeExecutableSchema({
   typeDefs,
---
+++ resolvers.js
@@ -0,0 +1,63 @@
+import { find } from 'lodash'
+import { authors, posts, agents } from './data'
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+    authors: () => authors,
+    agents: () => agents,
+    people: () => authors.concat(agents),
+    agent: (_, args) => find(agents,{ id: args.id }),
+    author: (_, args) => find(authors, { id: args.id }),
+  },
+
+  Mutation: {
+    createPost: (_, {input}) => {
+      // Build new post object from input values
+
+      // "id" is sequential index into "posts" array
+      let id = posts.length + 1
+
+      // Extract values from input data
+      let thisTitle = input["title"]
+      let thisArticleType = input["articleType"]
+      let thisAuthor = input["authorId"]
+
+      let newPost = {id: id, title: thisTitle, authorId: thisAuthor, articleType: thisArticleType}
+      posts.push(newPost)
+      // Notify subscribers
+      pubsub.publish(POST_ADDED_TOPIC, { postAdded: newPost });  // publish to a topic
+      return newPost
+      },
+    },
+
+  Subscription: {
+    postAdded: {
+      subscribe: () => pubsub.asyncIterator(POST_ADDED_TOPIC)  // subscribe to new posts
+    }
+  },
+
+  Author: {
+    posts: (author) => filter(posts, { authorId: author.id }),
+  },
+  Post: {
+    author: (post) => find(authors, { id: post.authorId }),
+  },
+  Agent: {
+    represents: (agent) => filter(authors, { agent: agent})
+  },
+	Person: {
+    // This code differentiates between the two implementations of Person
+    // It is required because objects returned from the "people" query
+    //    must be defined types, not interface types
+    __resolveType(data, context, info) {
+		  if (data.agent) {
+        return 'Author'
+      } else {
+        return 'Agent'
+      }
+    }
+  },
+}
+
+export { resolvers }
