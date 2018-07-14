ac8eb213e655ec59eae28696d6466b3205be987b
Brian Capouch <brianc@palaver.net>
2018-07-14 02:15:27 -0400

    Modify to do graphQL hello query

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import http from 'http'
 
-// The simplest express server on earth
+import { graphql, buildSchema } from 'graphql'
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// The canonical simplest graphQL server there ever was
 var app = express();
 const server = http.createServer(app)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 server.listen(4000);
 console.log(
