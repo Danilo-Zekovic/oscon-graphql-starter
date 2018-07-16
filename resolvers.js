903adfad5c4806b5df283467271f2ffcbaaf4fd2
Brian Capouch <brianc@palaver.net>
2018-07-16 05:54:08 -0400

    Add resolvers for base schema

---
+++ resolvers.js
@@ -0,0 +1,19 @@
+import { find } from 'lodash'
+import { authors, posts } from './data'
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+    authors: () => authors,
+    author: (_, args) => find(authors, { id: args.id }),
+    post: (_, args) => find(posts, { id: args.id })
+  },
+  Author: {
+    posts: (author) => filter(posts, { authorId: author.id }),
+  },
+  Post: {
+    author: (post) => find(authors, { id: post.authorId }),
+  },
+};
+
+export { resolvers }
