582d738730e5585c0e719dff0a763ae83f92f11c
Brian Capouch <brianc@palaver.net>
2018-07-17 06:02:59 -0400

    Add new file for classroom use

--- resolvers.js
+++ resolvers.js
@@ -5,29 +5,11 @@ const resolvers = {
   Query: {
     posts: () => posts,
     authors: () => authors,
-    agents: () => agents,
-    people: () => authors.concat(agents),
-    agent: (_, args) => find(agents,{ id: args.id }),
     author: (_, args) => find(authors, { id: args.id }),
     post: (_, args) => find(posts, { id: args.id })
   },
   Author: {
     posts: (author) => filter(posts, { author: author }),
-  },
-  Agent: {
-    represents: (agent) => filter(authors, { agent: agent})
-  },
-		Person: {
-      // This code differentiates between the two implementations of Person
-      // It is required because objects returned from the "people" query
-      //    must be defined types, not interface types
-      __resolveType(data, context, info) {
-  		  if (data.agent) {
-          return 'Author'
-        } else {
-          return 'Agent'
-        }
-      }
   },
 }
 
