b0b9e5291bd3fe084645e2b5d599d5d2c5f8a6d7
Brian Capouch <brianc@palaver.net>
2018-07-16 22:24:31 -0400

    Rejigger to eliminate kludge

--- resolvers.js
+++ resolvers.js
@@ -10,10 +10,7 @@ const resolvers = {
   },
   Author: {
     posts: (author) => filter(posts, { authorId: author.id }),
-  },
-  Post: {
-    author: (post) => find(authors, { id: post.authorId }),
-  },
+  }
 }
 
 export { resolvers }
