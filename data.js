2ab7551a6416020b363af873c5118722becdd1ae
Brian Capouch <brianc@palaver.net>
2018-07-17 02:23:03 -0400

    Fix data file

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
-import { find } from 'lodash'
+import { find, filter } from "lodash"
 
-// Mock database ====
+// "Mock" database is simply programmer-deined data ====
 const agents = [
   { id: 1, firstName: 'Larry', middleInitial: 'L', lastName: 'Thomas'},
   { id: 2, firstName: 'Bill', middleInitial: 'R', lastName: 'Lewis' }
 ]
 
 const authors = [
-  { id: 1, firstName: 'Tom', middleInitial: 'W',lastName: 'Coleman'},
-  { id: 2, firstName: 'Sashko', middleInitial: 'L', lastName: 'Stubailo'},
-  { id: 3, firstName: 'Mikhail', middleInitial: 'R', lastName: 'Novikov' },
-]
+  { id: 1, firstName: 'Tom', middleInitial: 'W',lastName: 'Coleman', agent: find(agents,{id: 1})},
+  { id: 2, firstName: 'Sashko', middleInitial: 'L', lastName: 'Stubailo', agent: find(agents,{id: 2})},
+  { id: 3, firstName: 'Mikhail', middleInitial: 'R', lastName: 'Novikov', agent: find(agents, {id: 1}) },
+];
 
 const posts = [
   { id: 1, author: find(authors, { id: 1 }), title: 'Introduction to GraphQL',articleType: 'REVIEW' },
